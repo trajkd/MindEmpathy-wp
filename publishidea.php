7fa3c29d4fdf9a33efdc9e04d5d1e0f45ed083e4
trajkd <trajkd@gmail.com>
2020-10-10 09:54:56 +0200

    fix: echo IDs 3

--- publishidea.php
+++ publishidea.php
@@ -12,8 +12,8 @@
 		while (have_posts()) : the_post(); 
 		$IDs[] = str_getcsv(get_the_content());
 		endwhile;
-		echo $IDs[0];
-		echo $IDs[1];
+		print_r($IDs[0]);
+		print_r(IDs[1]);
 		if ($IDs[1] === esc_html( $current_user->user_email )) {
 			header( "Location: /viewmode/"+$IDs[0] ); die;
 		}; ?>
