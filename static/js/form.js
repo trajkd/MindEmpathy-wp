b7dc58f2e29ebabf40e69f30090f4bba7e78b18f
trajkd <trajkd@gmail.com>
2021-01-02 22:05:05 +0100

    fix: revert to use json

--- static/js/form.js
+++ static/js/form.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
 			files: files,
 			captcha: grecaptcha.getResponse()
 		},
-		dataType: 'jsonp',
+		dataType: 'json',
 		beforeSubmit: function() {
 			if(!$form.valid()) {
 				return false;
